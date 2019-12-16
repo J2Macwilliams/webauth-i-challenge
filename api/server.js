7a132dc23bd164a77a6d4621029e72a5dd2f9c36
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-16 11:45:21 -0800

    Create server.js

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const helmet = require('helmet');
+
+// Require router
+const authRouter = require('./auth/authRouter');
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+
+// Global test endpoint
+server.get('/', (req, res) => {
+res.send(`<h3>Let's Get into this App!</h3>`)
+});
+
+// Call carRouter
+server.use('/api/auth', authRouter);
+
+module.exports = server;

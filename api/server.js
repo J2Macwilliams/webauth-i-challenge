cb2c30f54dce50e60e324865294ef60f0235e4ab
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-16 15:54:51 -0800

    Change endpoint path.

--- api/server.js
+++ api/server.js
@@ -7,14 +7,16 @@ const authRouter = require('../auth/authRouter');
 
 const server = express();
 server.use(helmet());
+
 server.use(express.json());
 server.use(cors());
+
 // Global test endpoint
 server.get('/', (req, res) => {
 res.send(`<h3>Let's Get into this App!</h3>`)
 });
 
 // Call carRouter
-server.use('/api/', authRouter);
+server.use('/api', authRouter);
 
 module.exports = server;

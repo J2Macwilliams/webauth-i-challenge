de8c4b0e9814b43329b06d65699605236139ae0b
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-16 12:21:32 -0800

    Require bcrypt and restricted into authRouter.

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const dB = require('../models/authDb');
+const restricted = require('../middleware/restricted');
 
 const router = express.Router();
 
 router.post('/register', (req, res) => {
     
+
 });
 
 router.post('/login', (req, res) => {
     
+
 });
 
 router.get('/users', (req, res) => {
     
+
 });
 
 

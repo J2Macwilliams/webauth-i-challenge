ce755dc2e4caa91212050521fc126592875984d9
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-16 14:59:35 -0800

    Add start to package.json. Add CORS.

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 // Require router
 const authRouter = require('../auth/authRouter');
@@ -7,7 +8,7 @@ const authRouter = require('../auth/authRouter');
 const server = express();
 server.use(helmet());
 server.use(express.json());
-
+server.use(cors());
 // Global test endpoint
 server.get('/', (req, res) => {
 res.send(`<h3>Let's Get into this App!</h3>`)

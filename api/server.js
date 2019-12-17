c52903b8c1e488bfaa6bc86811340f90e50517e2
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-16 16:07:24 -0800

    Remove helmet

--- api/server.js
+++ api/server.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const helmet = require('helmet');
 const cors = require('cors');
 
 // Require router
 const authRouter = require('../auth/authRouter');
 
 const server = express();
-server.use(helmet());
 
 server.use(express.json());
 server.use(cors());
+
+
+// Call authRouter
+server.use('/api', authRouter);
+
 
 // Global test endpoint
 server.get('/', (req, res) => {
 res.send(`<h3>Let's Get into this App!</h3>`)
 });
 
-// Call carRouter
-server.use('/api', authRouter);
+
 
 module.exports = server;
